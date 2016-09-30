17a9d49b23d22180d5ed12fe8776c86abef4e936
lenni <lenni31@neuf.fr>
2016-09-30 11:21:09 +0200

    vertion final defis greeting

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('have fun !');
+$("button").click(function(){
+  var first = $('input[name=first_name]').val();
+  var last = $('input[name=last_name]').val();
+  var city =$('input[name="city').val();
+  var final =$('#username').html(first+" "+" "+ last+" "+ "de"+" " +city);
+});
+
